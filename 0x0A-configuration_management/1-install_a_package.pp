b8b75f048f0ef9ced2ca0b1a76946ab4ed7d42d2
Mona-shabka <mona.shabka@yahoo.com>
2024-02-23 04:07:48 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Read: http://puppet-lint.com/checks/
+# Using Puppet, install puppet-lint.
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
